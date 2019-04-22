d4c7516f11629719b68a12aa4a80a8abb1d7e045
Aleksandr <38788523+sansanich83@users.noreply.github.com>
2019-04-22 22:24:47 +0300

    Убирает связь tasks users

--- schema.sql
+++ schema.sql
@@ -29,17 +29,14 @@ COLLATE='utf8_general_ci'
 CREATE TABLE `tasks` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`task_name` VARCHAR(50) NOT NULL,
-	`user_id` INT(11) NOT NULL,
 	`category_id` INT(11) NOT NULL,
 	`date_create` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`status_complete` TINYINT(1) NULL DEFAULT NULL,
 	`file_link` VARCHAR(50) NULL DEFAULT NULL,
 	`deadline` DATE NOT NULL,
 	PRIMARY KEY (`id`),
-	INDEX `FK_tasks_users` (`user_id`),
 	INDEX `FK_tasks_categories` (`category_id`),
-	CONSTRAINT `FK_tasks_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
-	CONSTRAINT `FK_tasks_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+	CONSTRAINT `FK_tasks_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
