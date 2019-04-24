97ff8274f491c989b129dc6c2d6e8a9ade73e38f
Aleksandr <38788523+sansanich83@users.noreply.github.com>
2019-04-24 17:37:35 +0300

    исправляет ошибку с точной с запятой

--- queries.sql
+++ queries.sql
@@ -14,20 +14,21 @@ INSERT INTO `doingsdone`.`tasks` (`task_name`, `category_id`, `date_create`, `de
 INSERT INTO `doingsdone`.`tasks` (`task_name`, `category_id`, `date_create`, `deadline`) VALUES ('Выполнить тестовое задание', '3', '2019-04-23 16:06:27', '2018-12-21');
 INSERT INTO `doingsdone`.`tasks` (`task_name`, `category_id`, `date_create`, `deadline`) VALUES ('Сделать задание первого раздела', '2', '2019-04-23 16:07:27', '2018-12-21');
 INSERT INTO `doingsdone`.`tasks` (`task_name`, `category_id`, `date_create`, `deadline`) VALUES ('Собеседование в IT компании', '1', '2019-04-23 16:03:39', '2019-05-20');
-INSERT INTO `doingsdone`.`tasks` (`task_name`, `category_id`, `date_create`, `deadline`) VALUES ('Купить комп для кота', '4', '2019-04-23 16:07:27', '2019-04-23');
+INSERT INTO `doingsdone`.`tasks` (`task_name`, `category_id`, `date_create`, `deadline`) VALUES ('Купить корм для кота', '4', '2019-04-23 16:07:27', '2019-04-23');
 INSERT INTO `doingsdone`.`tasks` (`task_name`, `category_id`, `date_create`, `deadline`) VALUES ('Заказать пиццу', '4', '2019-04-23 16:10:43', '2019-04-23');
 
 -- Пометить задачу как выполненную
-UPDATE `doingsdone`.`tasks` SET `status_complete`='1' WHERE  `id`=7;
+UPDATE `doingsdone`.`tasks` SET `status_complete`='1' WHERE  `id`=3;
 
 -- Обновить название задачи по её идентификатору
-UPDATE `doingsdone`.`tasks` SET `task_name`='Купить комп для кота и пса' WHERE  `id`=8;
+UPDATE `doingsdone`.`tasks` SET `task_name`='Купить корм для кота и пса' WHERE  `id`=5;
 
 -- Список из всех категорий(проектов) для одного пользователя,  имя категории(проекта)
 SELECT category_name, task_name FROM categories c
 LEFT JOIN tasks t
 ON c.id = t.category_id
--- WHERE c.user_id = 1
+WHERE c.user_id = 1
+;
 
 -- Список из всех категорий(проектов) для одного пользователя, Список всех категорий и количество задач в каждой категории
 SELECT category_name, count(task_name) AS num_task FROM categories c
@@ -36,7 +37,7 @@ ON c.id = t.category_id
 -- WHERE c.user_id = 1
 GROUP BY category_name;
 
--- Список из всех задач для одного проекта
+-- Список из всех задач для одной категории (проекта)
 SELECT category_name,task_name FROM categories c
 INNER JOIN tasks t
 ON c.id = t.category_id
